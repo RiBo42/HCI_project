bcefb71ded5de1dd77b269ac140aaa1f5bcfaeda
RichardBozko <2472271b@student.gla.ac.uk>
2022-11-17 21:26:01 +0000

    JS & template upades
    Added fully functioning date&time range picker
    Began work on charting functions
    Updated index.html accordingly

--- main/views.py
+++ main/views.py
@@ -5,3 +5,7 @@ def index(request):
 	context = {}
 	return render(request, 'index.html', context)
 
+
+#def chart_data(request):
+
+
---
+++ static/js/scripts.js
@@ -0,0 +1,65 @@
+var charts = [ undefined, undefined, undefined ];\
+var testing_data = []
+function make_chart(id, data, canvas,chart_type) {
+    var ctx = document.getElementById(canvas);
+    if (charts[id] != undefined) {
+        charts[id].destroy();
+    }
+
+    charts[id] = new Chart(ctx, {
+        type: chart_type,
+        data: data,
+        options: {
+            plugins: {
+                legend: {
+                    labels: {
+                        color: "#F7F9F9",
+                        font: {
+                            size: 12
+                        }
+                    }
+                }
+            },
+            scales: {
+                y: {
+                    ticks: {
+                        color: "#F7F9F9",
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: "#F7F9F9",
+                    }
+                },
+            }
+        }
+    });
+}
+
+function update_chart(){
+    var range = $('.datetimerange').data('daterangepicker');
+    //console.log(range.startDate.format('YYYY-MM-DD HH:MM'))
+    $.ajax({
+        url: "/data/",
+        data: jQuery.param({
+            start: range.startDate.format('YYYY-MM-DD HH:MM'),
+            end: range.endDate.format('YYYY-MM-DD HH:MM'),
+        }),
+    });
+}
+
+$(function() {   
+   $('.datetimerange').daterangepicker({
+        timePicker: true,
+        startDate: moment().startOf('hour').subtract(24, 'hour'),
+        endDate: moment().startOf('hour'),
+        timePickerIncrement: 30,
+        locale: {
+            format: 'MM/DD/YYYY h:mm A'
+        }
+    });
+    $('.datetimerange').on('apply.daterangepicker', function(ev,picker){
+        update_chart();
+    });
+});
+$(document).ready(update_chart)
